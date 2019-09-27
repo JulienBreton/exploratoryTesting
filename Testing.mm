33b2a669d38615cdceb1b36897e9ad00114a075d
JulienBreton <julienbreton@hotmail.fr>
2019-09-27 16:50:32 +0200

    chore: add mind map testing

---
+++ Testing.mm
@@ -0,0 +1,205 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Testing" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1522573341" CREATED="1568975481282" MODIFIED="1569403782814"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="definition" COLOR="#18898b">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10" RULE="ON_BRANCH_CREATION"/>
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_1306264337" CREATED="1568981266878" MODIFIED="1569403682849"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="3">Exploratory testing</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#7c0000"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sources:
+    </p>
+    <p>
+      https://www.satisfice.com/blog/archives/1509
+    </p>
+    <p>
+      https://www.satisfice.com/download/session-based-test-management
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Definition" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1469202862" CREATED="1569244377344" MODIFIED="1569403682847" HGAP_QUANTITY="12.500000044703482 pt" VSHIFT_QUANTITY="-112.49999664723882 pt">
+<node LOCALIZED_STYLE_REF="default" ID="ID_242646339" CREATED="1568981280970" MODIFIED="1569403851532" HGAP_QUANTITY="44.749999083578615 pt" VSHIFT_QUANTITY="14.24999957531692 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#000000" face="Serif" size="2">A style of testing that emphasizes the freedom and responsibility of the individual tester to continually optimize the quality of his work by treating test design, test execution, test result interpretation, and learning as mutually supporting activities that continue in parallel throughout the course of the project</font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Source : https://www.satisfice.com/blog/archives/1509
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="What we can do?" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1120618084" CREATED="1569244291942" MODIFIED="1569403015767">
+<node LOCALIZED_STYLE_REF="default" ID="ID_924784097" CREATED="1569227298090" MODIFIED="1569403892003"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="2">We find bug fast</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1471260975" CREATED="1569244149230" MODIFIED="1569244224675"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="2">Continually adjust our plans to refo- </font>
+    </p>
+    <p>
+      <font size="2">cus on the most promising risk areas</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font SIZE="12"/>
+</node>
+<node TEXT="We follow hunches" ID="ID_1411332472" CREATED="1569244260566" MODIFIED="1569244267856"/>
+<node TEXT="We minimize the time spent on documentation" ID="ID_1444977087" CREATED="1569244351120" MODIFIED="1569244356175"/>
+</node>
+<node TEXT="Testing in Sessions" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_10631513" CREATED="1569247896846" MODIFIED="1569403018820">
+<node TEXT="A session is an uninterrupted block of reviewable,&#xa;chartered test effort." ID="ID_1116962280" CREATED="1569250289189" MODIFIED="1569250315036">
+<node TEXT="chartered = each session is associated with a mission&#x2014;what we are testing, or what problems we are looking for" ID="ID_934189285" CREATED="1569250663555" MODIFIED="1569250815879">
+<node TEXT="For example, analyze a function, or look for a particular problem, or verify a set of bug fixes." ID="ID_1760243420" CREATED="1569402725925" MODIFIED="1569402820766"/>
+</node>
+<node TEXT="uninterrupted = no significant interruptions&#x2014;no email, meetings, chatting, or telephone calls" ID="ID_663016843" CREATED="1569250695872" MODIFIED="1569250746487">
+<node TEXT="(90 minutes) We don&#x2019;t time them very strictly because we don&#x2019;t want to be more obsessed with time than with good testing." ID="ID_1041860588" CREATED="1569402625950" MODIFIED="1569402658808"/>
+</node>
+<node TEXT="reviewable = a report (called a session sheet) that provides information about what happened, in a format that can be examined by a third party (such as the test manager)." ID="ID_1027583763" CREATED="1569250758403" MODIFIED="1569251619038"/>
+</node>
+<node TEXT="Why?" ID="ID_940948004" CREATED="1569317224765" MODIFIED="1569317243309">
+<node TEXT="We needed a way to distinguish testing from&#xa;everything else" ID="ID_558528204" CREATED="1569248482646" MODIFIED="1569248487761"/>
+<node TEXT="Keeping track of each tester&#x2019;s progress" ID="ID_6664036" CREATED="1569245178558" MODIFIED="1569317243307" HGAP_QUANTITY="37.249999307096026 pt" VSHIFT_QUANTITY="1.4999999552965178 pt">
+<node TEXT="What we test?" ID="ID_1097014804" CREATED="1569245189597" MODIFIED="1569245722049"/>
+<node TEXT="What we found?" ID="ID_1620048043" CREATED="1569245220762" MODIFIED="1569245726179"/>
+<node TEXT="What our priorities are for further&#xa;testing?" ID="ID_468249668" CREATED="1569245229125" MODIFIED="1569245728597"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Testing is the process of evaluating a product by learning about it through exploration and experimentation, which includes: questioning, study, modeling, observation and inference, output checking, etc." LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_752220745" CREATED="1569403758085" MODIFIED="1569403841978" HGAP_QUANTITY="-174.24999438971298 pt" VSHIFT_QUANTITY="-210.74999371916076 pt">
+<edge COLOR="#007c7c"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Source : https://www.satisfice.com/blog/archives/1509
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</map>
